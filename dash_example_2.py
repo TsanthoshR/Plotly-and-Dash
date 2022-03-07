6b7120a13fa87d80389a997c0033b00a2c655866
TSanthosh <santhosht5290@gmail.com>
2022-03-08 00:16:24 +0530

    Chain Callback 1

--- dash_example_2.py
+++ dash_example_2.py
@@ -1,65 +1,83 @@
 import dash
 from dash import html
 from dash import dcc
-from dash.dependencies import Input,Output
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 
-happiness = pd.read_csv('./downloads/world_happiness.csv')
+happiness = pd.read_csv("./downloads/world_happiness.csv")
 
-Regions = [{'label':i, 'value':i} for i in happiness['region'].unique()]
-Country = [{'label':i, 'value':i} for i in happiness['country'].unique()]
+Regions = [{"label": i, "value": i} for i in happiness["region"].unique()]
+# Country = [{"label": i, "value": i} for i in happiness["country"].unique()]
 # line_fig = px.line()
 app = dash.Dash()
 
 # app.layout = html.Div("My Dashboard 1")
-app.layout = html.Div([
-    html.H1('World Happiness Dashboard'),
-    html.P(['World Happiness Report Data Sourse',
-            html.Br(),
-            html.A('World Happiness Report Data Sourse',
-                href="https://worldhappiness.report",
-                target="_blank")
+app.layout = html.Div(
+    [
+        html.H1("World Happiness Dashboard"),
+        html.P(
+            [
+                "World Happiness Report Data Sourse",
+                html.Br(),
+                html.A(
+                    "World Happiness Report Data Sourse",
+                    href="https://worldhappiness.report",
+                    target="_blank",
+                ),
+            ]
+        ),
+        dcc.RadioItems(id="Region_Radio", options=Regions, value="North America"),
+        # dcc.Checklist(options=Regions, value=["North America"]),
+        dcc.Dropdown(id="Country_dropdown", 
+        # options=Country,  CallBack included
+        value="United States" 
+        ),
+        dcc.RadioItems(
+            id="Graph_select",
+            options=[
+                {"label": "Happiness Score", "value": "happiness_score"},
+                {"label": "Happiness Rank", "value": "happiness_rank"},
+            ],
+            value="happiness_score",
+        ),
+        dcc.Graph(id="Happiness_Graph"),
+        html.Div(id="avg_index"),
+    ]
+)
 
-            ]),
-    dcc.RadioItems(options=Regions,
-                   value = 'North America'),
-    dcc.Checklist(options=Regions,
-                  value=['North America']),
-    dcc.Dropdown(id="Country_dropdown",
-                 options=Country, 
-                 value ='United States'),
 
-    dcc.RadioItems(id="Graph_select",
-                   options=[
-                       {'label':'Happiness Score','value':'happiness_score'}, 
-                       {'label':'Happiness Rank','value':'happiness_rank'}, 
-                             ],
-                   value="happiness_score"),
-                
-    dcc.Graph(id = "Happiness_Graph" ),
-    html.Div(id="avg_index")
+@app.callback(Output("Country_dropdown", "options"), Input("Region_Radio", "value"))
+def updateDropdown(selcted_region):
+    Country_options = [
+        {"label": i, "value": i}
+        for i in happiness[happiness["region"] == selcted_region]["country"].unique()
+    ]
 
-])
+    return Country_options
+
 
 @app.callback(
-    Output("Happiness_Graph",'figure'),
-    Output("avg_index","children"),
-    
-    Input(component_id="Country_dropdown",component_property='value'),
-    Input("Graph_select",'value'),
+    Output("Happiness_Graph", "figure"),
+    Output("avg_index", "children"),
+    Input(component_id="Country_dropdown", component_property="value"),
+    Input("Graph_select", "value"),
 )
-def updateGraph(selected_country,selected_graph):
+def updateGraph(selected_country, selected_graph):
 
-    df_filtered = happiness[happiness['country']==selected_country]
-    line_fig =px.line(df_filtered,
-                      x='year',
-                      y=selected_graph,
-                      title=f'{selected_graph} in {selected_country}')
-    avg_val = df_filtered[selected_graph].mean()  
-    return line_fig, f"the average value of {selected_graph} for {selected_country} is {avg_val}"
+    df_filtered = happiness[happiness["country"] == selected_country]
+    line_fig = px.line(
+        df_filtered,
+        x="year",
+        y=selected_graph,
+        title=f"{selected_graph} in {selected_country}",
+    )
+    avg_val = df_filtered[selected_graph].mean()
+    return (
+        line_fig,
+        f"the average value of {selected_graph} for {selected_country} is {avg_val}",
+    )
 
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-
