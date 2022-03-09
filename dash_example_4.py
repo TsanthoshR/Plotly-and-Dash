de51b776c1b46f619f392f6d3ede7f05cae52818
TSanthosh <santhosht5290@gmail.com>
2022-03-10 00:09:29 +0530

    Grid Components

--- dash_example_4.py
+++ dash_example_4.py
@@ -11,28 +11,32 @@ soccer = pd.read_csv("./downloads/fifa_soccer_players.csv")
 player_names = [{'label' : i , 'value' : i } for i in soccer['long_name'].unique()]
 
 app = dash.Dash(__name__,
-                external_stylesheets = [dbc.themes.CYBORG])
+                external_stylesheets = [dbc.themes.GRID])
 
 app.layout = html.Div([
     dcc.Location(id = 'url',pathname="/test"),
-    html.H1('Soccer Players Dashboard',
-            # style={
-            #         'textAlign' : 'Centre',
-            #         'fontFamily' : 'fantasy',
-            #         'fontSize' : 'blue'
-            # }
-            ),
-    html.P(['Source: ',
-            html.A('Sofifa',
-                    href = 'https://sofifa.con/',
-                    target = '_blank')],
-                    # style= {'border':'solid'}
-                     ),
-    html.Label('Player name : '),
-    dcc.Dropdown(options = player_names,
-                 value = player_names[0]['value'],
-                #  style={'backgroundColor':'lightblue'}
-                 )
+    html.H1('Soccer Players Dashboard'),
+    dbc.Row([
+        dbc.Col([
+            html.P(['Source: ',
+                html.A('Sofifa',
+                        href = 'https://sofifa.con/',
+                        target = '_blank')],
+                        # style= {'border':'solid'}
+                        ),
+        ]),
+        dbc.Col([
+            html.Label('Player name : '),
+            dcc.Dropdown(options = player_names,
+                            value = player_names[0]['value'],
+                            #  style={'backgroundColor':'lightblue'}
+                        )
+
+        ])
+    ])
+    
+    
+    
 ],
 # style={'padding':100, 'border':'solid'}
 )
